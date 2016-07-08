fb25d31fc6aba5256a08a11156f9922141c6306b
Morgan Bauer <mbauer@us.ibm.com>
2016-07-08 12:50:03 -0700

    an example of third party resource creation and instantiation using only kubectl and yaml definitions

---
+++ k8s/third-party-resource-example/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o xtrace
+set -o errexit
+
+kubectl create -f tpr-definition.yaml
+sleep 15 # the create needs some time to be propagated
+kubectl get thirdpartyresources
+kubectl get thirdpartyresources --output=yaml
+kubectl get thirdpartyresources abc.tuv.xyz --output=yaml
+# kubectl get thirdpartyresources Abc --output=yaml
+kubectl get Abc --output=yaml
+sleep 1
+kubectl create -f tpr-instance.yaml
+sleep 4
+kubectl get Abc my-new-abc-object --output=yaml
+
+sleep 1 
+kubectl delete -f tpr-definition.yaml
