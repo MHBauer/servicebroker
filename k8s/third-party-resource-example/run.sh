1b40c48db76cb2902a85f1b6781be0b1a33faa5f
Morgan Bauer <mbauer@us.ibm.com>
2016-07-08 13:04:03 -0700

    delete instance example

--- k8s/third-party-resource-example/run.sh
+++ k8s/third-party-resource-example/run.sh
@@ -14,6 +14,7 @@ sleep 1
 kubectl create -f tpr-instance.yaml
 sleep 4
 kubectl get Abc my-new-abc-object --output=yaml
-
+sleep 4
+kubectl delete Abc my-new-abc-object
 sleep 1 
 kubectl delete -f tpr-definition.yaml
