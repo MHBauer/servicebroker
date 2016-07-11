e65e366ae0c98a08af8e45786c45b3ce20de5d34
Morgan Bauer <mbauer@us.ibm.com>
2016-07-11 12:33:51 -0700

    whoaoaoa

--- k8s/third-party-resource-example/run.sh
+++ k8s/third-party-resource-example/run.sh
@@ -4,17 +4,30 @@ set -o xtrace
 set -o errexit
 
 kubectl create -f tpr-definition.yaml
+kubectl create -f tpr2-definition.yaml
 sleep 15 # the create needs some time to be propagated
 kubectl get thirdpartyresources
 kubectl get thirdpartyresources --output=yaml
 kubectl get thirdpartyresources abc.tuv.xyz --output=yaml
 # kubectl get thirdpartyresources Abc --output=yaml
+sleep 5
 kubectl get Abc --output=yaml
+kubectl get Qrs --output=yaml
 sleep 1
 kubectl create -f tpr-instance.yaml
+kubectl create -f tpr-instance2.yaml
+kubectl create -f tpr2-instance.yaml
+kubectl create -f tpr2-instance2.yaml
 sleep 4
 kubectl get Abc my-new-abc-object --output=yaml
+kubectl get Abc my-2-abc-object --output=yaml
+kubectl get Qrs my-new-abc-object --output=yaml
+kubectl get Qrs my-2-abc-object --output=yaml
+
+
 sleep 4
 kubectl delete Abc my-new-abc-object
+kubectl delete Abc my-2-abc-object
 sleep 1 
 kubectl delete -f tpr-definition.yaml
+kubectl delete -f tpr2-definition.yaml
